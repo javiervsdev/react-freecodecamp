3599411cf5bb3e0277179b56922ab8c42d786ff7
javiervs.dev <javiervs.dev@gmail.com>
2022-06-11 15:52:36 +0200

    Add css to format web structure and component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Testimonio from './components/Testimonio';
 
 function App() {
-  return (
-    <div className="App">
-      <Testimonio />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="contenedor-principal">
+                <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp:</h1>
+                <Testimonio/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
