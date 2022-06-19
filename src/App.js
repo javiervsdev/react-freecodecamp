72d5fa885b86024af176d3a0e1d3f65d2ad93499
javiervs.dev <javiervs.dev@gmail.com>
2022-06-19 11:31:43 +0200

    Add logic to update click number to buttons

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import logo from './images/click-logo.png'
 import Boton from "./components/Boton";
 import Contador from "./components/Contador";
+import {useState} from "react";
 
 function App() {
 
+    const INITIAL_NUM_CLICKS = 0;
+
+    const [numClicks, setNumClicks] = useState(INITIAL_NUM_CLICKS);
+
     const addClickHandler = () => {
-        console.log("Click");
+        setNumClicks(numClicks + 1);
     }
 
     const resetCounterHandler = () => {
-        console.log("Reiniciar");
+        setNumClicks(INITIAL_NUM_CLICKS);
     }
 
     return (
@@ -22,7 +27,7 @@ function App() {
                 <span className='logo-nombre-app'>Click-me</span>
             </div>
             <div className='contenedor-principal'>
-                <Contador contadorValue='0' />
+                <Contador contadorValue={numClicks} />
                 <Boton
                     texto='Click'
                     esBotonDeClick={true}
